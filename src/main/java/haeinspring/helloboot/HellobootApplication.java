be9024e5e8b5498a80c95f2cc323aac618432038
haein-side <kimhi1640@gmail.com>
2023-06-28 20:00:53 +0900

    독립 실행 가능한 서블릿 컨테이너 + 서블릿 완료

--- src/main/java/haeinspring/helloboot/HellobootApplication.java
+++ src/main/java/haeinspring/helloboot/HellobootApplication.java
@@ -23,6 +23,8 @@ public class HellobootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			servletContext.addServlet("hello", new HttpServlet() {
+				HelloController helloController = new HelloController();
+
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 					// 인증, 보안, 다국어, 공통 기능
@@ -30,9 +32,13 @@ public class HellobootApplication {
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
 						String name = req.getParameter("name");
 
+						// 바인딩 - 평범한 자바 타입(String)으로 웹 요청 변환 -> 새로운 형태의 타입으로 변환하고 파라미터로 넘기는 것
+						// 웹 요청 직접적으로 엑세스 하는 프론트 컨트롤러와 같은 코드에서 처리하는 객체에게 평범한 데이터 타입으로 변환해서 넘겨주는 것
+						String ret = helloController.hello(name);
+
 						resp.setStatus(HttpStatus.OK.value());
 						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println("Hello " + name);
+						resp.getWriter().println(ret);
 					}
 					else if (req.getRequestURI().equals("/user")) {
 
