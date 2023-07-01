99e6836f49af01d6c8292c3a3776b93207b46b92
haein-side <kimhi1640@gmail.com>
2023-07-01 13:24:58 +0900

    meta annotation

--- src/test/java/haeinspring/helloboot/HelloServiceTest.java
+++ src/test/java/haeinspring/helloboot/HelloServiceTest.java
@@ -1,7 +1,27 @@
 package haeinspring.helloboot;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Test
+@interface UnitTest {
+
+}
 
 public class HelloServiceTest {
     @Test
@@ -12,5 +32,14 @@ public class HelloServiceTest {
         String ret = helloService.sayHello("Test");
 
         Assertions.assertThat(ret).isEqualTo("Hello Test");
+    }
+
+    @Test
+    void helloDecorator() {
+        HelloDecorator helloDecorator = new HelloDecorator(name -> name);
+
+        String ret = helloDecorator.sayHello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("*Test*");
     }
 }
