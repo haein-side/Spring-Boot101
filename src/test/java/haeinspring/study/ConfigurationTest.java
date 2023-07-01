3a05e058373dd47680454ae48acf4a3eaa10af27
haein-side <kimhi1640@gmail.com>
2023-07-02 03:40:37 +0900

    프록시 true 일 땐 @Configuration 싱글톤으로 관리

--- src/test/java/haeinspring/study/ConfigurationTest.java
+++ src/test/java/haeinspring/study/ConfigurationTest.java
@@ -16,8 +16,33 @@ public class ConfigurationTest {
         Bean1 bean1 = ac.getBean(Bean1.class);
         Bean2 bean2 = ac.getBean(Bean2.class);
 
+        // @Configuration이 붙은 클래스가 스프링 컨테이너에서 사용될 때 오브젝트가 하나만 생성됨
+        // @Configuration proxy = true -> proxy object 1개 생성 및 bean 등록
         Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
 
+    @Test
+    void proxyCommonMethod() {
+        // 직접 프록시를 만들어서 스프링 컨테이너 안에서 일어나는 일을 흉내내 본 것
+        // common method가 생성하는 개수를 하나로 제한, 재사용할 수 있게 캐싱
+        // 프록시 오브젝트를 통해 오브젝트 하나로 관리
+        // 스프링 컨테이너가 프록시 클래스 생성하고 @Configuration 붙은 빈 오브젝트로 관리하는 것
+        MyConfigProxy myConfigProxy = new MyConfigProxy();
+
+        Bean1 bean1 = myConfigProxy.bean1();
+        Bean2 bean2 = myConfigProxy.bean2();
+
+        Assertions.assertThat(bean1.common).isSameAs(bean2.common);
+    }
+
+    static class MyConfigProxy extends MyConfig {
+        private Common common;
+        @Override
+        Common common() {
+            if (this.common == null) this.common = super.common();
+
+            return this.common;
+        }
     }
 
     @Configuration
