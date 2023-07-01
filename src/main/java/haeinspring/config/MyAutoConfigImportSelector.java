d18f1e0d0ec9164c35853a853b78e2a476d01b42
haein-side <kimhi1640@gmail.com>
2023-07-01 16:47:07 +0900

    자동 구성 애노테이션 적용

--- src/main/java/haeinspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/haeinspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,28 @@
 package haeinspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "haeinspring.config.autoconfig.DispatcherServletConfig",
-                "haeinspring.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        // 자동구성에 사용할 configuration class들의 목록이 candidates에 들어가 있을 것
+        // Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        // Iterable이기 떄문에 for each 사용가능
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
--- src/main/java/haeinspring/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/haeinspring/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,11 @@
 package haeinspring.config.autoconfig;
 
+import haeinspring.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
